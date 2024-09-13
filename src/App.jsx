e5e433cba348d5ce13342ebe168b53f6ff3989b1
Micah Mustaham <micahmustaham@gmail.com>
2024-09-13 11:30:06 +0800

    Initial commit [project setup]

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navbar from "./sections/Navbar.jsx";
+
+const App = () => {
+    return (
+        <main className="max-w-7xl mx-auto">
+           <Navbar />
+        </main>
+    )
+}
+export default App
---
+++ src/sections/Navbar.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
+            <div className="max-w-7xl mx-auto">
+                <div className="flex justify-between items-center py-5 mx-auto c-space">
+                    <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transtion-colors">
+                        Micah Mustaham
+                    </a>
+                    <button>
+                        <img src="" alt=""/>
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
+}
+export default Navbar
