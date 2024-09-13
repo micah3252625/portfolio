9d6480b713f4a0450bf9c8c72891899b62fd6589
Micah Mustaham <micahmustaham@gmail.com>
2024-09-13 12:00:32 +0800

    Implemented the navbar section

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,7 +1,25 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
+import React, {useState} from 'react'
+import {navLinks} from "../constants/index.js";
+
+const NavItems = () => {
+    return (
+        <ul className="nav-ul ">
+            {navLinks.map(({id, href, name}) => (
+                <li key={id} className="nav-li">
+                    <a href={href} className="nav-li_a" onClick={() => {}}>
+                        {name}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    )
+}
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleMenu = () => setIsOpen((prevIsOpen) => !prevIsOpen)
     return (
         <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
             <div className="max-w-7xl mx-auto">
@@ -9,10 +27,18 @@ const Navbar = () => {
                     <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transtion-colors">
                         Micah Mustaham
                     </a>
-                    <button>
-                        <img src="" alt=""/>
+                    <button onClick={toggleMenu} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex" aria-label="Toggle Menu">
+                        <img src={isOpen ? "assets/close.svg" : "assets/menu.svg"} alt="toggle" className="w-6 h-6" />
                     </button>
+                    <nav className="sm:flex hidden">
+                        <NavItems />
+                    </nav>
                 </div>
+            </div>
+            <div className={`nav-sidebar ${isOpen ? 'max-h-screen' : 'max-h-0'}`}>
+                <nav className="p-5">
+                    <NavItems />
+                </nav>
             </div>
         </header>
     )
